7c4bee55ce6141275b40d352ecd7ee029534c5d1
USER <n15dcpt017@student.ptithcm.edu.vn>
2021-05-25 13:34:45 +0700

    init tikishopee

---
+++ assets/css/main.js
@@ -0,0 +1,14 @@
+
+<script>
+    var x = document.getElementById("js_login");
+    var y = document.getElementById("js_register");
+
+    function login() {
+        x.style.left = "-600px",
+        y.style.left = "50px"
+    }
+    function register() {
+        y.style.left = "-600px",
+        x.style.left = "50px"
+    }
+</script>
